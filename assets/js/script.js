3132f64a373118695baf7c962570a9152619c084
Francesco  Vitale <francescovitale@MacBook-Air-di-Francesco.local>
2023-12-11 19:08:49 +0100

    Move HTML content dynamically using JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,182 @@
-const title = document.querySelector('#glass');
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('content-container');
+
+  //start header
+  const header = document.createElement('header');
+  header.id = 'myHeader';
+  header.className = 'fixed-top';
+
+  const nav = document.createElement('nav');
+  nav.className = 'navbar bg-body-tertiary';
+
+  const containerFluid = document.createElement('div');
+  containerFluid.className = 'container-fluid';
+
+  const navbarBrand = document.createElement('span');
+  navbarBrand.className = 'navbar-brand mb-0 h1 mx-auto d-block';
+  navbarBrand.textContent = 'HydratioMate';
+
+  container.appendChild(header);
+  header.appendChild(nav);
+  nav.appendChild(containerFluid);
+  containerFluid.appendChild(navbarBrand);
+  //end header
+
+  //start glasses tracker
+  const mainPlayContainer = document.createElement('div');
+  mainPlayContainer.className = 'container-fluid mb-5';
+  container.appendChild(mainPlayContainer);
+
+  const rowContainer = document.createElement('div');
+  rowContainer.className = 'row align-items-center';
+  mainPlayContainer.appendChild(rowContainer);
+
+  const glassTrackerContainer = document.createElement('div');
+  glassTrackerContainer.className = 'col-12 col-md-12 col-lg-4 d-flex justify-content-center glass-container';
+  rowContainer.appendChild(glassTrackerContainer);
+
+  const subGlassTrackerContainer = document.createElement('div');
+  subGlassTrackerContainer.className = 'd-flex flex-column align-items-center text-center';
+  glassTrackerContainer.appendChild(subGlassTrackerContainer);
+
+  const cupImg = document.createElement('img');
+  cupImg.className = 'img-fluid mx-auto picture';
+  cupImg.src = 'assets/images/cup.svg';
+  cupImg.alt = 'cup';
+  subGlassTrackerContainer.appendChild(cupImg);
+
+  const zeroElement = document.createElement('p');
+  zeroElement.id = 'glasses-tracker';
+  zeroElement.textContent = '0';
+  subGlassTrackerContainer.appendChild(zeroElement);
+  //end glasses tracker
+
+  //start HydratioMate Assistant
+  const assistantContainer = document.createElement('div');
+  assistantContainer.className = 'col-12 col-md-12 col-lg-4 d-flex flex-column justify-content-center mt-5 mb-5 card-btn align-items-center text-center';
+  rowContainer.appendChild(assistantContainer);
+
+  const h1 = document.createElement('h1')
+  h1.id = 'title';
+  h1.textContent = 'HydratioMate Assistant';
+  assistantContainer.appendChild(h1);
+
+  const subAssistantContainer = document.createElement('div')
+  subAssistantContainer.className = 'd-flex flex-column align-items-center';
+  assistantContainer.appendChild(subAssistantContainer);
+
+  const counterElement = document.createElement('p')
+  counterElement.id = 'glass';
+  counterElement.className = 'mb-3';
+  counterElement.textContent = '0';
+  subAssistantContainer.appendChild(counterElement);
+
+  const buttonContainer = document.createElement('div')
+  buttonContainer.className = 'd-flex align-items-center';
+  subAssistantContainer.appendChild(buttonContainer);
+
+  const incrementButton = document.createElement('button');
+  incrementButton.id = 'increment';
+  incrementButton.className = 'btn btn-primary mb-2 me-4';
+  incrementButton.textContent = '+ Glass';
+
+  const resetButton = document.createElement('button');
+  resetButton.id = 'reset';
+  resetButton.className = 'btn btn-secondary mb-2 custom-btn';
+  resetButton.textContent = 'Reset';
+
+  const decrementButton = document.createElement('button');
+  decrementButton.id = 'decrement';
+  decrementButton.className = 'btn btn-primary mb-2  ms-4';
+  decrementButton.textContent = '- Glass';
+
+  buttonContainer.appendChild(incrementButton);
+  buttonContainer.appendChild(resetButton);
+  buttonContainer.appendChild(decrementButton);
+   //end HydratioMate Assistant
+
+   //start litres tracker
+  const litresTrackerContainer = document.createElement('div');
+  litresTrackerContainer.className = 'col-12 col-md-12 col-lg-4 d-flex justify-content-center glass-container';
+  rowContainer.appendChild(litresTrackerContainer);
+
+  const subLitresTrackerContainer = document.createElement('div');
+  subLitresTrackerContainer.className = 'd-flex flex-column align-items-center text-center';
+  litresTrackerContainer.appendChild(subLitresTrackerContainer);
+
+  const dropImg = document.createElement('img');
+  dropImg.className = 'img-fluid mx-auto picture';
+  dropImg.src = 'assets/images/drop.svg';
+  dropImg.alt = 'drop';
+  subLitresTrackerContainer.appendChild(dropImg);
+
+  const litresElement = document.createElement('p');
+  litresElement.id = 'liters-tracker';
+  litresElement.textContent = '0';
+  subLitresTrackerContainer.appendChild(litresElement);
+   //end litres tracker
+
+  //start footer
+   const footerContainer = document.createElement('div');
+   footerContainer.className = 'container my-5 footer';
+   container.appendChild(footerContainer);
+
+
+   // Create row div
+   const row = document.createElement('div');
+   row.classList.add('row');
+   footerContainer.appendChild(row);
+
+   // Create first column
+   const col1 = document.createElement('div');
+   col1.classList.add('col-md-6', 'col-12', 'd-flex', 'justify-content-md-start', 'justify-content-center');
+   row.appendChild(col1);
+
+   // Create trademark link
+   const trademarkLink = document.createElement('a');
+   trademarkLink.classList.add('footerTrademark');
+   trademarkLink.href = 'https://www.vitalefrancesco.com/';
+   trademarkLink.target = '_blank';
+   trademarkLink.textContent = '© Vitale Francesco';
+   col1.appendChild(trademarkLink);
+
+   // Create second column
+   const col2 = document.createElement('div');
+   col2.classList.add('col-md-6', 'col-12', 'd-flex', 'justify-content-md-end', 'justify-content-center');
+   row.appendChild(col2);
+
+   // Create navigation div
+   const navFooter = document.createElement('nav');
+   nav.classList.add('footerLogos');
+   col2.appendChild(navFooter);
+
+   // Create Instagram link and image
+   createSocialLink('instagram-footer', 'https://www.instagram.com/vitale_francesco_/', 'assets/images/instagram-icon.svg', 'Instagram');
+
+   // Create LinkedIn link and image
+   createSocialLink('linkedin-footer', 'https://www.linkedin.com/in/francesco-vitale-206765226/', 'assets/images/linkedin-icon.svg', 'LinkedIn');
+
+   // Create GitHub link and image
+   createSocialLink('git-footer', 'https://github.com/francescovitale-dev', 'assets/images/github-icon.svg', 'GitHub');
+
+   // Function to create social links
+   function createSocialLink(className, href, imgSrc, altText) {
+    const link = document.createElement('a');
+    link.classList.add(className);
+    link.href = href;
+    link.target = '_blank';
+  
+    const img = document.createElement('img');
+    img.classList.add('footer-picture-' + className.split('-')[0]);
+    img.src = imgSrc;
+    img.alt = altText;
+  
+    link.appendChild(img);
+    navFooter.appendChild(link);
+  }  
+  //end footer
+
+  const title = document.querySelector('#glass');
 const glasses = document.querySelector('#glasses-tracker');
 const liters = document.querySelector('#liters-tracker'); 
 let glass = localStorage.getItem('glass') ? parseInt(localStorage.getItem('glass')) : 0;
@@ -118,6 +296,8 @@ document.querySelector('#reset').onclick = reset;
 
 ifGlasses();
 updateDecrementButtonState();
+
+  }); 
 
 
 
